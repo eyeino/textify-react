0c9f603c421dae55ad1404d0b1c3fc9be3afd68f
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2019-03-31 17:05:05 -0400

    refactor: App.js from class to function

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MusicInfo from './musicInfo';
 
-class App extends Component {
-  render() {
-    const trackDetails = {
-      artistName: 'Toro Y Moi',
-      albumName: 'Outer Peace',
-      trackName: 'Ordinary Pleasure',
-      albumImgUrl: 'https://i.scdn.co/image/bc37539ee4ad2d9bc3fe51d96cbf443f0d44aece',
-      trackLink: 'https://open.spotify.com/track/31F0KxmTD4rz3o0tJht5RL'
-    }
-
-    return (
-      <div>
-        <MusicInfo trackDetails={trackDetails} />
-      </div>
-    );
+function App() {
+  const trackDetails = {
+    artistName: 'Toro Y Moi',
+    albumName: 'Outer Peace',
+    trackName: 'Ordinary Pleasure',
+    albumImgUrl: 'https://i.scdn.co/image/bc37539ee4ad2d9bc3fe51d96cbf443f0d44aece',
+    trackLink: 'https://open.spotify.com/track/31F0KxmTD4rz3o0tJht5RL'
   }
+
+  return (
+    <div>
+      <MusicInfo trackDetails={trackDetails} />
+    </div>
+  );
 }
 
 export default App;
